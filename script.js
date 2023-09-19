772b291584ffff281d3613f8650d8d1650d0c0d7
Camila Villanueva <129124701+camilayelenvillanueva@users.noreply.github.com>
2023-09-18 21:14:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function evaluateExpression() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
